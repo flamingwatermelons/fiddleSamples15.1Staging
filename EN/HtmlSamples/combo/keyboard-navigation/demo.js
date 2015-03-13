7cd7545d881fd447f15d3896baaa4cfd02331316
unknown <OctopusStaging@S0106WEB3S.IGWEB.LOCAL>
2015-03-13 13:42:19 -0400

    SB build 15.1.20151.92

--- EN/HtmlSamples/combo/keyboard-navigation/demo.js
+++ EN/HtmlSamples/combo/keyboard-navigation/demo.js
@@ -1,14 +1,6 @@
 $(function () {
             var tipIndex = 0;
 
-            $("#tryAgaintBtn")
-                .button()
-                .click(function (event, sender) {
-                    startExample();
-                    $(this).hide();
-                })
-                .hide();
-
             $("#combo").igCombo({
                 width: "270px",
                 textKey: "MountainName",
@@ -19,32 +11,35 @@ $(function () {
                 },
                 dropDownOpened: function (evt, ui) {
                     if ((evt.keyCode == 40) && (tipIndex == 1)) {
-                        showWellDoneMessage(true);
+                        showWellDoneMessage();
                     }
                 },
                 dropDownClosed: function (evt, ui) {
-                    if ((evt.keyCode == 38) && (tipIndex == 2)) {
-                        showWellDoneMessage(true);
+                    if ((evt.keyCode == 38) && (tipIndex == 4)) {
+                        showWellDoneMessage();
                     }
 
-                    if (evt.keyCode == 27 && tipIndex == 3) {
-                        showWellDoneMessage(true);
+                    //Close the drop down when there are selected items
+                    if (evt.keyCode == 27 && tipIndex == 7 && $("#combo").igCombo("selectedItems")) {
+                        showWellDoneMessage();
                     }
                 },
                 selectionChanged: function (evt, ui) {
                     if (evt.keyCode == 16 && (tipIndex == 6)) {
-                        //stop examples and restart tipIndex
-                        showWellDoneMessage(false);
-                        tipIndex = 0;
+                        showWellDoneMessage();
                     }
                 }
             });
 
+            var opened = false;
+
             $("#tooltip").tooltip({
-                position: { of: ".ui-igcombo-field", my: "left+55 center", at: "right center" },
+                position: { of: ".ui-igcombo-buttonicon", my: "left+55 center", at: "right center" },
                 open: function (event, ui) {
-                    debugger;
+                    //if (!opened) {
                     $("#tooltip").tooltip("option", "content", getTip(tipIndex++));
+                    opened = true;
+                    //}
                 },
                 tooltipClass: "tooltipStyle"
             });
@@ -55,24 +50,29 @@ $(function () {
 
 
             $(document).keydown(function (e) {
-                if (e.keyCode == 36 && e.ctrlKey && tipIndex == 4) {
+                if (e.keyCode == 36 && e.ctrlKey && tipIndex == 3) {
                     var ddOpened = $("#combo").igCombo("dropDownOpened");
 
                     if (ddOpened) {
-                        showWellDoneMessage(true);
+                        showWellDoneMessage();
                     } else {
                         showMessage("Open the Drop Down first!");
                     }
                 }
 
-                if (e.keyCode == 35 && e.ctrlKey && tipIndex == 5) {
+                if (e.keyCode == 35 && e.ctrlKey && tipIndex == 2) {
                     var ddOpened = $("#combo").igCombo("dropDownOpened");
 
                     if (ddOpened) {
-                        showWellDoneMessage(true);
+                        showWellDoneMessage();
                     } else {
                         showMessage("Open the Drop Down first!");
                     }
+                }
+
+                //Clear the input when Esc is selected
+                if (e.keyCode == 27 && tipIndex == 5) {
+                    showWellDoneMessage();
                 }
             });
 
@@ -92,33 +92,33 @@ $(function () {
                 }, 2000);
             }
 
-            function showWellDoneMessage(toContinue) {
-                $("div[role='tooltip']").addClass("glowing-border");
-                $("#tooltip").tooltip("option", "content", "Well done!!!");
+            function showWellDoneMessage() {
+                var content = $("#tooltip").tooltip("option", "content");
+                $("div[role='tooltip']").switchClass("normal", "glowing-border", 1000);
+                $("#tooltip").tooltip("option", "content", content + " <span style='display: inline-block' class='ui-icon ui-icon-check'></span>");
 
                 setTimeout(function () {
                     $("#tooltip").tooltip("close");
 
                     setTimeout(function () {
-                        if (toContinue) {
+                        if (tipIndex == 7) $("#tooltip").tooltip("close");
+                        else {
                             $("#tooltip").tooltip("open");
-                        } else {
-                            //show Try again button
-                            $("#tryAgaintBtn").show();
+                            $("#tooltip").tooltip("option", "position", { of: ".ui-igcombo-buttonicon", my: "left+55 center", at: "right center" });
                         }
-                    }, 1000);
+                    }, 500);
                 }, 1500);
             }
 
             function getTip(idx) {
                 var tips = [
-                    "Press <b>Down arrow</b> to Open drop down if closed or Move to next item if opened",
-                    "Press <b>Up arrow</b> to Close drop down if opened or Move to previous item",
-                    "1. Type some value for example 'text' and Press <b>Esc key</b> to clear the content when there isn't selected item; <br />" +
-                    "2. Now Press <b>Esc key</b> again in order to close the drop down",
-                    "Press <b>Ctrl</b> + <b>Home</b> to Move to very first item in drop down when Drop down is opened",
-                    "Press <b>Ctrl</b> + <b>End</b> to Move to very last item when Drop down is opened",
-                    "Press of <b>Shift</b> + <b>Up/Down arrows</b> will change the active (highlighted) items and if you release <b>Shift key</b> the active items will be selected",
+	"Press <b>Down arrow</b> to Open drop down if closed or Move to next item if opened",
+	"Press <b>Ctrl</b> + <b>End</b> to Move to very last item when Drop down is opened",
+	"Press <b>Ctrl</b> + <b>Home</b> to Move to very first item in drop down when Drop down is opened",
+	"Press <b>Up arrow</b> to Close drop down if opened or Move to previous item",
+	"Type some value, for example 'text' and Press <b>Esc key</b> to clear the content when there isn't selected item",
+	"While holding <b>Shift</b> + <b>Up/Down arrows</b> the active (highlighted) items will be changed, if you wamt to select them, just release <b>Shift key</b>",
+    "Press <b>Esc key</b> in order to close the drop down when item/items are selected",
                 ];
 
                 return tips[idx];
